1fb06df3c1819e9ed4cad3cfcf8e40233e8f5f4a
arash-malekzadeh <arash.malekzade@gmail.com>
2025-01-17 12:15:46 +0330

    useEffect2

--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -3,6 +3,9 @@ import ProductItem from "./components/Product-item";
 
 function ProductList({ name, city, listOfProducts }) {
   const [flag, setFlag] = useState(false);
+  const [count, setCount] = useState(0);
+  const [changeStyle, setChangeStyle] = useState(false);
+
   const handletoggleText = () => {
     setFlag(!flag);
   };
@@ -10,9 +13,26 @@ function ProductList({ name, city, listOfProducts }) {
   useEffect(() => {
     console.log("runs only once on page load");
   }, []);
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+  useEffect(() => {
+    if (count == 10) setChangeStyle(true);
+  }, [count]);
   return (
     <div>
       <h3> ecommerce project</h3>
+
+      <h2> Count is {count}</h2>
+      <button
+        onClick={increaseCount}
+        style={{
+          color: changeStyle ? "red" : "black",
+          background: changeStyle ? "blue" : "black",
+        }}
+      >
+        Increase Count
+      </button>
       <button onClick={handletoggleText}>toggle text</button>
       {flag ? (
         <h4>
